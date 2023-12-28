9e776b32f97ceabacf67d921d8114eee8e5efdc3
lifangqi <lifangqi@zhixueyun.com>
2023-12-28 16:15:41 +0800

    增加Vector动画实践相关代码

--- module_tech/src/main/java/com/silencefly96/module_tech/tech/demo/VectorAnimTestDemo.kt
+++ module_tech/src/main/java/com/silencefly96/module_tech/tech/demo/VectorAnimTestDemo.kt
@@ -4,11 +4,13 @@ package com.silencefly96.module_tech.tech.demo
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.Animatable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.silencefly96.module_base.base.BaseFragment
 import com.silencefly96.module_tech.databinding.FragmentVectorTestBinding
+
 
 class VectorAnimTestDemo: BaseFragment() {
 
@@ -22,8 +24,29 @@ class VectorAnimTestDemo: BaseFragment() {
 
     @SuppressLint("SetTextI18n")
     override fun doBusiness(context: Context?) {
+        // SVG轨迹动画，取巧用一下TextView的drawableBottomCompat:
+        val drawable = binding.animatedVectorPath.compoundDrawables[3] as Animatable
+        drawable.start()
+        binding.animatedVectorPath.setOnClickListener {
+            if (!drawable.isRunning) drawable.start()
+        }
 
+        // SVG路径动画，从一种形状变成另一种形状:
+        val drawable1 = binding.animatedVectorMorphing.compoundDrawables[3] as Animatable
+        drawable1.start()
+        binding.animatedVectorMorphing.setOnClickListener {
+            if (!drawable1.isRunning) drawable1.start()
+        }
 
+        // SVG多种动画，从一种形状变成另一种形状:
+        val drawable2 = binding.animatedVectorCombine.compoundDrawables[3] as Animatable
+        // 一开始有问题
+        binding.animatedVectorCombine.post {
+            drawable2.start()
+        }
+        binding.animatedVectorCombine.setOnClickListener {
+            if (!drawable2.isRunning) drawable2.start()
+        }
     }
 
     override fun onDestroyView() {
